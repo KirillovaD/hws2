621bf17ee818fe740d25990adaffebe17001e911
KirillovaD <sdaria@inbox.ru>
2023-02-03 17:07:27 +0300

    add hw9 clock change format

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -35,22 +35,21 @@ function Clock() {
     const onMouseLeave = () => {
         setShow(false)// пишут студенты // спрятать дату если мышка не наведена
     }
-    const stringTime = date.toLocaleString("ru", {
+    const stringTime = date.toLocaleString("ru-Ru", {
         hour: "numeric",
         minute: "numeric",
         second: "numeric"
     });  // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleString("ru", {
-        day: "numeric", month: "numeric", year: "numeric",
-    }) // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = date.toLocaleDateString('ru-Ru') // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleString("en", {
         weekday: "long"
     }) // пишут студенты
-    const stringMonth = date.toLocaleString("en", {
-        month: "long"
-    }) // пишут студенты*/
+    const stringMonth = new Intl.DateTimeFormat('en-US', {
+        month: 'long'
+    }).format(date)
+ // пишут студенты*/
 
     return (
         <div className={s.clock}>
